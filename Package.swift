04b3d10dc209088c1eddca71e16779d95678d075
niuhui <niuhui@zhihu.com>
2018-06-20 13:10:51 +0800

    Package

--- Package.swift
+++ Package.swift
@@ -4,14 +4,14 @@ import PackageDescription
 let package = Package(
     name: "WLoveServer",
     dependencies: [
-        // 💧 A server-side Swift web framework.
-        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
+        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.5"),
+        .package(url: "https://github.com/vapor/fluent-mysql.git", from: "3.0.0-rc.4.0.1"),
+        .package(url: "https://github.com/vapor/redis.git", from: "3.0.0-rc.3"),
+        .package(url: "https://github.com/vapor/leaf.git", from: "3.0.0-rc.2")
 
-        // 🔵 Swift ORM (queries, models, relations, etc) built on SQLite 3.
-        .package(url: "https://github.com/vapor/fluent-sqlite.git", from: "3.0.0-rc.2")
     ],
     targets: [
-        .target(name: "App", dependencies: ["FluentSQLite", "Vapor"]),
+        .target(name: "App", dependencies: ["FluentMySQL", "Vapor","Redis","Leaf"]),
         .target(name: "Run", dependencies: ["App"]),
         .testTarget(name: "AppTests", dependencies: ["App"])
     ]
