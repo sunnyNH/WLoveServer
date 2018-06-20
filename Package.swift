f57939cc570cc5f9e3643f58a7a4a022fc5da0ef
niuhui <niuhui@zhihu.com>
2018-06-20 14:48:13 +0800

    package

--- Package.swift
+++ Package.swift
@@ -8,7 +8,6 @@ let package = Package(
         .package(url: "https://github.com/vapor/fluent-mysql.git", from: "3.0.0-rc.4.0.1"),
         .package(url: "https://github.com/vapor/redis.git", from: "3.0.0-rc.3"),
         .package(url: "https://github.com/vapor/leaf.git", from: "3.0.0-rc.2")
-
     ],
     targets: [
         .target(name: "App", dependencies: ["FluentMySQL", "Vapor","Redis","Leaf"]),
